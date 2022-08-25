b48b7585e0ee3294f0c406786f731bf51198e575
Muhammed Shafeeque P <shafeequekkv95@gmail.com>
2022-08-25 22:34:11 +0530

    cross env enabled

--- Src/Config/db.js
+++ Src/Config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    
+    const conn = await mongoose.connect(process.env.NODE_ENV==="production"?process.env.MONGO_URI:process.env.MONGO_URL);
     console.log("MongoDB Connected");
   } catch (error) {
     console.log(error);
